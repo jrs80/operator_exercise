4497ca1c51333fe84d5459af587923b87ffd5071
JRS80 <jason.steffen@gmail.com>
2022-06-02 12:33:30 -0500

    upload operator exercise

---
+++ Operator Exercise/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace Operator_Exercise
+{
+    internal class Program
+    {
+        public static double AOC(double r) => Math.PI * r * r;
+        
+        static void Main(string[] args)
+        {
+            int x = 5, y = 3, q, r;
+            q = x / y;
+            r = x % y;
+            Console.WriteLine($"{x} / {y} is {q} remainder {r}");
+
+            Console.WriteLine("Enter radius: ");
+            double rad = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"The area of a circle with radius {rad} is " + AOC(rad));
+
+        }
+    }
+}
